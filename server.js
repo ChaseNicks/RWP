56f3461b21a59cb97b650bf13319d82e3b354df7
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-15 07:13:38 -0400

    :

--- server.js
+++ server.js
@@ -29,6 +29,14 @@ app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
