7965439bd2624ca6e21b19764ea3acf6f274ee95
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-15 07:09:39 -0400

    Added all the requirement to initialize the express / sequelize server

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
