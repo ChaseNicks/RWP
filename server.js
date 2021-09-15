f8394d07cbea5e97613b792af099e26b9e9def10
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-15 07:07:26 -0400

    Added all the required dependencies to the server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
