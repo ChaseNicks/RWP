13709e2ff691100d65758f90fdd10038eb79661e
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-17 12:35:41 -0400

    replaced project routes by the postRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
