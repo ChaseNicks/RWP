24777d59b78da66906e99314d26f8e5a3c25d6f7
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-18 14:27:44 -0400

    Added the import and export

--- jobs-api-testing/script.js
+++ jobs-api-testing/script.js
@@ -1,7 +1,11 @@
-fetch("https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=93a9f958&app_key=64741373e2fc20513e2967dc826628ff&results_per_page=10&category=it-jobs")
-  .then (response => response.json())
-  .then (data => console.log(data));
+fetch(
+  'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=93a9f958&app_key=64741373e2fc20513e2967dc826628ff&results_per_page=10&category=it-jobs',
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
-  fetch("http://api.adzuna.com/v1/api/jobs/us/history?app_id=93a9f958&app_key=64741373e2fc20513e2967dc826628ff&category=it-jobs&content-type=application/json")
-  .then (response => response.json())
-  .then (data => console.log(data));
+fetch(
+  'http://api.adzuna.com/v1/api/jobs/us/history?app_id=93a9f958&app_key=64741373e2fc20513e2967dc826628ff&category=it-jobs&content-type=application/json',
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
