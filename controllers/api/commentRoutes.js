bd9d034e5086cce81f2d9baf54a72c82c02fa4b6
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-19 08:30:18 -0400

    Added the get one comment by id

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,4 +2,30 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Gets all comments from the database
+router.get('/', async ({ res }) => {
+  try {
+    const dbCommentData = await Comment.findAll({});
+
+    res.json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
