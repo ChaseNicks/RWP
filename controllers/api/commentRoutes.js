a3cc46b62dd5d1b1490173d95236ab5beffc8b8e
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-19 08:39:51 -0400

    Added the put method that updates one comment based on the id

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -46,4 +46,29 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// Updates one comment based on the id
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const dbCommentData = await Comment.update(
+      {
+        comment: req.body.comment,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      },
+    );
+
+    if (!dbCommentData) {
+      res.status(404).json({ message: 'No comment found with this id' });
+      return;
+    }
+    res.json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
