bb5eab56cbae3292fa57001f56ef66e211562cf4
Beardo Mattix <christopher.mattix@gmail.com>
2021-09-19 14:33:44 -0400

    working on posts

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -4,18 +4,28 @@ const modalBg = document.querySelector('.modal-background');
 const modalCloseBtn = document.querySelector('#modal-close-btn');
 
 const postFormEl = document.querySelector('#new-post-form');
+newPostBtn.addEventListener('click', () => {
+  postModal.classList.add('is-active');
+});
+modalBg.addEventListener('click', () => {
+  postModal.classList.remove('is-active');
+});
 
 const newFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const content = document.querySelector('input[name="content"]').value;
+  console.log(title);
+  const content = document.querySelector('input[name="post-content"]').value;
+  console.log(content);
+  const tag = document.querySelector('input[name="post-tags"]:checked').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
       title,
       content,
+      tag,
     }),
     headers: {
       'Content-Type': 'application/json',
@@ -34,9 +44,4 @@ document
   .querySelector('#new-post-form')
   .addEventListener('submit', newFormHandler);
 
-newPostBtn.addEventListener('click', () => {
-  postModal.classList.add('is-active');
-});
-modalBg.addEventListener('click', () => {
-  postModal.classList.remove('is-active');
-});
+// console.log(tag);
