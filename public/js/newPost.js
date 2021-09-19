4304ffcdd1f583fc5e202ec4a4a50dc04b0977a1
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-19 12:58:56 -0400

    Added the new post logic

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -5,6 +5,35 @@ const modalCloseBtn = document.querySelector('#modal-close-btn');
 
 const postFormEl = document.querySelector('#new-post-form');
 
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const content = document.querySelector('input[name="content"]').value;
+
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  console.log(response);
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document
+  .querySelector('#new-post-form')
+  .addEventListener('submit', newFormHandler);
+
 newPostBtn.addEventListener('click', () => {
   postModal.classList.add('is-active');
 });
